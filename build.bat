75a22ca4b11e8ffb7f108733cb44257ffbff61a6
Rexagon <reide740@gmail.com>
2019-08-07 16:35:58 +0300

    Добавил скрипты для сборки

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+call clean.bat
+
+PUSHD %CD%
+call prepare.bat
+POPD
+
+mkdir cmakefiles_gcc
+cd cmakefiles_gcc
+
+mkdir programmer
+cd programmer
+
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ../../
+cmake --build .
