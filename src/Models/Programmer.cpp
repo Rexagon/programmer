b08c914210006c485405e8218577d325047bd232
Rexagon <reide740@gmail.com>
2019-08-15 15:02:19 +0300

    Добавил методы для работы с сервисным регистром

--- src/Models/Programmer.cpp
+++ src/Models/Programmer.cpp
@@ -4,8 +4,11 @@
  */
 #include "Programmer.h"
 
+#include <cassert>
+#include <iomanip>
 #include <iostream>
 
+#include <sitl/commands/Iden.h>
 #include <sitl/commands/List.h>
 #include <sitl/commands/Mwr.h>
 
@@ -18,6 +21,52 @@ constexpr auto ADDRESS_REG = 0x080002u;
 
 constexpr auto BIVK_DATA_BEGIN = 0x040000u;
 constexpr auto BIVK_DATA_END = 0x07FFFFu;
+
+void setBits(uint16_t &target, uint16_t value, uint16_t valueMask, uint16_t offset)
+{
+    const auto mask = static_cast<uint16_t>(~static_cast<uint16_t>(valueMask << offset));
+    target &= mask;
+    target |= static_cast<uint16_t>(static_cast<uint16_t>(value & valueMask) << offset);
+}
+
+namespace sr
+{
+constexpr uint16_t EXT_RES_O = 0u;
+constexpr uint16_t EXT_RES_I = 1u;
+constexpr uint16_t BUS_OE = 2u;
+constexpr uint16_t RESET = 3u;
+
+} // namespace sr
+
+namespace timings
+{
+struct Range
+{
+    bool check(const app::Programmer::TimingValue &timing) const
+    {
+        return timing.value >= min && timing.value <= max;
+    }
+
+    void write(uint16_t &target, const app::Programmer::TimingValue &timing) const
+    {
+        assert(check(timing));
+        setBits(target, timing.value, 0x0003u, offset);
+    }
+
+    uint16_t offset;
+
+    uint16_t min;
+    uint16_t max;
+};
+
+constexpr auto WRITE_SETUP = Range{4u /* offset */, 1u /* min */, 4u /* max */};
+constexpr auto WRITE_ACTIVE = Range{6u /* offset */, 3u /* min */, 6u /* max */};
+constexpr auto WRITE_HOLD = Range{8u /* offset */, 1u /* min */, 4u /* max */};
+
+constexpr auto READ_SETUP = Range{10u /* offset */, 1u /* min */, 4u /* max */};
+constexpr auto READ_ACTIVE = Range{12u /* offset */, 4u /* min */, 7u /* max */};
+constexpr auto READ_HOLD = Range{14u /* offset */, 2u /* min */, 4u /* max */};
+} // namespace timings
 } // namespace
 
 
@@ -26,11 +75,14 @@ namespace app
 Programmer::Programmer(const std::string &port, unsigned int baudRate)
     : m_connection{port, baudRate, true}
 {
+    // Проверяем соединение
     m_connection.setResponseTimeout(2);
-
     m_connection.execute<sitl::cmds::List>();
 
-    m_connection.execute<sitl::cmds::Mwr<uint32_t, uint16_t>>(SERVICE_REG, 0xFFFFu);
+    // Получаем описание устройства
+    //m_description = m_connection.execute<sitl::cmds::Iden>();
+
+    reset();
 }
 
 
@@ -39,5 +91,63 @@ Programmer::~Programmer()
     m_connection.execute<sitl::cmds::Mwr<uint32_t, uint16_t>>(SERVICE_REG, 0x0000u);
 }
 
+
+void Programmer::reset()
+{
+    // Устанавливаем тайминги
+    setWritingTimings(2_T, 4_T, 3_T);
+    setReadingTimings(1_T, 7_T, 2_T);
+
+    // Включаем буффер
+    setBuffersEnabled(true);
+
+    // Применяем конфигурацию
+    applyConfiguration();
+}
+
+
+void Programmer::setBuffersEnabled(bool enabled)
+{
+    setBits(m_serviceReg, enabled, 0x0001u, sr::BUS_OE);
+}
+
+
+void Programmer::setWritingTimings(const Programmer::TimingValue &setup,
+                                   const Programmer::TimingValue &active,
+                                   const Programmer::TimingValue &hold)
+{
+    using namespace timings;
+
+    WRITE_SETUP.write(m_serviceReg, setup);
+    WRITE_ACTIVE.write(m_serviceReg, active);
+    WRITE_HOLD.write(m_serviceReg, hold);
+}
+
+
+void Programmer::setReadingTimings(const app::Programmer::TimingValue &setup,
+                                   const app::Programmer::TimingValue &active,
+                                   const app::Programmer::TimingValue &hold)
+{
+    using namespace timings;
+
+    READ_SETUP.write(m_serviceReg, setup);
+    READ_ACTIVE.write(m_serviceReg, active);
+
+    auto readHold = hold;
+    ++readHold.value;
+    READ_HOLD.write(m_serviceReg, readHold);
+}
+
+
+const std::string &Programmer::getDescription() const
+{
+    return m_description;
+}
+
+
+void Programmer::applyConfiguration()
+{
+    m_connection.execute<sitl::cmds::Mwr<uint32_t, uint16_t>>(SERVICE_REG, m_serviceReg);
+}
 
 } // namespace app
--- src/Models/Programmer.h
+++ src/Models/Programmer.h
@@ -8,14 +8,35 @@ namespace app
 class Programmer final
 {
 public:
+    struct TimingValue
+    {
+        uint16_t value;
+    };
+
     explicit Programmer(const std::string &port, unsigned int baudRate);
     ~Programmer();
+
+    void reset();
+
+    void setBuffersEnabled(bool enabled);
+    void setWritingTimings(const TimingValue &setup, const TimingValue &active, const TimingValue &hold);
+    void setReadingTimings(const TimingValue &setup, const TimingValue &active, const TimingValue &hold);
+
+    void applyConfiguration();
+
+    const std::string &getDescription() const;
 
 private:
     sitl::Connection m_connection;
 
     std::string m_description{};
+    uint16_t m_serviceReg = 0x0000u;
 };
+
+constexpr Programmer::TimingValue operator""_T(unsigned long long value)
+{
+    return Programmer::TimingValue{static_cast<uint8_t>(value)};
+}
 
 } // namespace app
 
